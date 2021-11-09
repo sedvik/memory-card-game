b76b55946bc7125a56558c58b1ea94c50e58fb78
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-09 16:36:28 -0700

    Remove props from MemoryGame component instance in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
 import MemoryGame from './components/MemoryGame'
-import cards from './util/cardData'
 
 function App () {
   return (
     <div className="App">
       <Header />
-      <MemoryGame
-        cards={cards}
-        gameIsOver={false}
-        currentScore={3}
-        bestScore={7}
-      />
+      <MemoryGame />
     </div>
   )
 }
