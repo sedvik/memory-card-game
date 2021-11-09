b0f4b817c8ada9c42ed9bae1047364ca90b5d6ce
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-09 10:53:02 -0700

    Add App.css to App.js imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import MemoryGame from './components/MemoryGame'
+import cards from './util/cardData'
+import './styles/App.css'
 
 function App () {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <MemoryGame
+        cards={cards}
+        gameIsOver={true}
+        currentScore={3}
+        bestScore={7}
+      />
     </div>
   )
 }
